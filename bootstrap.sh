6114d1b3850efd77df8510ae30ae1b5d8acca561
Diego Allen <diegoallen@gmail.com>
2024-01-13 12:14:28 -0300

    Bootstrap the db and scrape web archive versions

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# scrape site from the web archive from 2023-09-03
+scrape_url=https://web.archive.org/web/20230309222819/http://www.nomina.itaipu.info/ output_filename_template=nomina_itaipu_2023-09-03.csv python main.py
+python create_or_update_db.py nomina_itaipu_2023-09-03.csv
+
+# scrape site from the web archive from 2023-05-08
+scrape_url=https://web.archive.org/web/20230508021538/http://nomina.itaipu.info/ output_filename_template=nomina_itaipu_2023-05-08.csv python main.py
+python create_or_update_db.py nomina_itaipu_2023-05-08.csv
+
+# scrape site from the web archive from 2023-05-16
+scrape_url=https://web.archive.org/web/20230516195208/http://nomina.itaipu.info/ output_filename_template=nomina_itaipu_2023-05-16.csv python main.py
+python create_or_update_db.py nomina_itaipu_2023-05-16.csv
+
+python main.py
+python create_or_update_db.py
